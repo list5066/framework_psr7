f2eda25f6a1177459bad0b1c7e01d9a934c36b34
list5066 <5066@list.ru>
2018-06-10 21:36:52 +0600

    assoc request 4 getLang

--- public/index.php
+++ public/index.php
@@ -2,17 +2,37 @@
 // some comment here, and link to memorize url of lessons https://youtu.be/w4iqxN0nfTs
 // Eliseev Dmitrii lesson about psr7 framework making
 
-function getLang($default)
+/**
+ * @param array $request
+ * @param $default
+ * @return bool|string
+ */
+function getLang(array $request, $default)
 {
+	$get = $request['get'];
+	$cookie = $request['cookie'];
+	$session = $request['session'];
+	$server = $request['server'];
+
 	return
-		!empty($_GET['lang']) ? $_GET['lang'] :
-			(!empty($_COOKIE['lang'])) ? $_COOKIE['lang'] :
-				(!empty($_SESSION['lang'])) ? $_SESSION['lang'] :
-					(!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
+		!empty($get['lang']) ? $get['lang'] :
+			(!empty($cookie['lang'])) ? $cookie['lang'] :
+				(!empty($session['lang'])) ? $session['lang'] :
+					(!empty($server['HTTP_ACCEPT_LANGUAGE'])) ? substr($server['HTTP_ACCEPT_LANGUAGE'], 0, 2) :
 		$default;
 }
 
+session_start();
+
+$request = [
+	'get' => $_GET,
+	'cookie' => $_COOKIE,
+	'session' => $_SESSION,
+	'server' => $_SERVER,
+];
+
 $name = $_GET['name'] ?? 'Guest';
+$lang = getLang($request, 'en');
 
 header('X-Developer: list5066');
 echo 'Hello, ', $name,'!';
