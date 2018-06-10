977ca49dc70b2d03f95d713f098b3b4ab9d82272
list5066 <5066@list.ru>
2018-06-10 22:18:45 +0600

    few methods 4 Request class

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -9,21 +9,49 @@ namespace Framework\Http;
 
 class Request
 {
+	/**
+	 * @return array
+	 */
 	public function getQueryParams(): array
 	{
 		return $_GET;
 	}
 
+	/**
+	 * @return array
+	 */
+	public function getParsedBody(): array
+	{
+		return $_POST ?: null;
+	}
+
+	/**
+	 * @return bool|string
+	 */
+	public function getBody()
+	{
+		return file_get_contents('php://input');
+	}
+
+	/**
+	 * @return array
+	 */
 	public function getCookies(): array
 	{
 		return $_COOKIE;
 	}
 
+	/**
+	 * @return array
+	 */
 	public function getSession(): array
 	{
 		return $_SESSION;
 	}
 
+	/**
+	 * @return array
+	 */
 	public function getServer(): array
 	{
 		return $_SERVER;
